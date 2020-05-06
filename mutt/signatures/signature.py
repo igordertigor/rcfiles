c58dbe5e673821bc8aa9a0dacc5ac460d5ca37bc
Ingo Fruend <git@ingofruend.net>
2020-05-06 11:34:35 -0400

    adding mutt config

---
+++ mutt/signatures/signature.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import random
+
+signatures = [
+    "www.ingostarr.de",
+    "http://soundcloud.com/ingo-starr/ingo-starr-bad-holiday",
+    "http://soundcloud.com/ingo-starr/10-favoriten",
+    "http://www.mixcloud.com/Ingo_Starr/ingo-starr-10-more/",
+    "http://www.mixcloud.com/Ingo_Starr/10-selected/",
+    "http://soundcloud.com/ingo-starr/ingo-starr-smooth-joy",
+    "http://soundcloud.com/ingo-starr/ingo-starr-pop-it",
+    "http://soundcloud.com/ingo-starr/ingo-starr-inter",
+    "http://soundcloud.com/ingo-starr/ingo-starr-aint-no-other-mama",
+    "http://soundcloud.com/ingo-starr/sets/ultrasonic-dancefloor-shit/",
+    "http://www.mixcloud.com/Ingo_Starr/the-musical-roundabout/",
+    "https://soundcloud.com/ingo-starr/ingo-starr-chant",
+    ]
+
+print(random.choice(signatures))
