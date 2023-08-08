f82af973aa62015c2a57bd08983f5b45a7a6270e
Ingo <ingo@verbally.de>
2023-08-08 10:08:46 +0200

    nvim mappings for various symbols

--- nvchad-custom/chadrc.lua
+++ nvchad-custom/chadrc.lua
@@ -11,4 +11,5 @@ M.ui = {
   }
 }
 M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
 return M
---
+++ nvchad-custom/mappings.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.abc = {
+  i = {
+    ["|>"] = {"↦", "maps to arrow", opts = {nowait = true}},
+    ["->"] = {"→", "right arrow", opts = {nowait = true}},
+    ["~>"] = {"⇝", "leads to arrow", opts = {nowait = true}},
+    ["=>"] = {"⇒", "implies arrow", opts = {nowait = true}},
+    [",o"] = {"☐", "checkbox empty", opts = {nowait = true}},
+    [",v"] = {"☑", "checkbox checked", opts = {nowait = true}},
+    [",x"] = {"☒", "checkbox crossed", opts = {nowait = true}},
+  }
+}
+
+return M
