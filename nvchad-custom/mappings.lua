7e82b79574f45b49a371693ce4e420be667e0320
Ingo <ingo@verbally.de>
2023-08-09 09:46:03 +0200

    Unicode mappings prefixed by @

--- nvchad-custom/mappings.lua
+++ nvchad-custom/mappings.lua
@@ -2,13 +2,13 @@ local M = {}
 
 M.abc = {
   i = {
-    ["|>"] = {"↦", "maps to arrow", opts = {nowait = true}},
-    ["->"] = {"→", "right arrow", opts = {nowait = true}},
-    ["~>"] = {"⇝", "leads to arrow", opts = {nowait = true}},
-    ["=>"] = {"⇒", "implies arrow", opts = {nowait = true}},
-    [",o"] = {"☐", "checkbox empty", opts = {nowait = true}},
-    [",v"] = {"☑", "checkbox checked", opts = {nowait = true}},
-    [",x"] = {"☒", "checkbox crossed", opts = {nowait = true}},
+    ["@|>"] = {"↦", "maps to arrow", opts = {nowait = true}},
+    ["@->"] = {"→", "right arrow", opts = {nowait = true}},
+    ["@~>"] = {"⇝", "leads to arrow", opts = {nowait = true}},
+    ["@=>"] = {"⇒", "implies arrow", opts = {nowait = true}},
+    ["@o"] = {"☐", "checkbox empty", opts = {nowait = true}},
+    ["@v"] = {"☑", "checkbox checked", opts = {nowait = true}},
+    ["@x"] = {"☒", "checkbox crossed", opts = {nowait = true}},
   }
 }
 
