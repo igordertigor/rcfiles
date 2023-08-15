0982288eba46a1cae0f6d41850cacdd4a5960148
Ingo <ingo@verbally.de>
2023-08-15 09:17:15 +0200

    Check off functionality

--- nvchad-custom/mappings.lua
+++ nvchad-custom/mappings.lua
@@ -1,6 +1,22 @@
 local M = {}
 
 M.abc = {
+  n = {
+    ["@v"] = {
+      function ()
+        vim.cmd(":s/☐/☑/")
+        vim.cmd(":nohl")
+      end,
+      "check"
+    },
+    ["@x"] = {
+      function ()
+        vim.cmd(":s/☐/☒/")
+        vim.cmd(":nohl")
+      end,
+      "cross"
+    },
+  },
   i = {
     ["@|>"] = {"↦", "maps to arrow", opts = {nowait = true}},
     ["@->"] = {"→", "right arrow", opts = {nowait = true}},
@@ -9,7 +25,7 @@ M.abc = {
     ["@o"] = {"☐", "checkbox empty", opts = {nowait = true}},
     ["@v"] = {"☑", "checkbox checked", opts = {nowait = true}},
     ["@x"] = {"☒", "checkbox crossed", opts = {nowait = true}},
-  }
+  },
 }
 
 return M
