19e521297515ab306f474a57eb5a7a2d8f2858e8
Ingo <ingo@verbally.de>
2023-08-17 15:28:41 +0200

    Figured out the buffer settings

--- nvchad-custom/init.lua
+++ nvchad-custom/init.lua
@@ -8,21 +8,13 @@ local autocmd = vim.api.nvim_create_autocmd
 autocmd("FileType", {
   callback = function()
     if vim.bo.ft == "markdown" then
-      vim.keymap.set("i", "->", "→", {nowait = true})
-      vim.keymap.set("i", "~>", "⇝", {nowait = true})
-      vim.keymap.set("i", "=>", "⇒", {nowait = true})
-      vim.keymap.set("i", "|>", "↦", {nowait = true})
-      vim.keymap.set("i", "@o", "☐", {nowait = true})
-      vim.keymap.set("i", "@v", "☑", {nowait = true})
-      vim.keymap.set("i", "@x", "☒", {nowait = true})
-    else
-      vim.keymap.del("i", "->")
-      vim.keymap.del("i", "~>")
-      vim.keymap.del("i", "=>")
-      vim.keymap.del("i", "|>")
-      vim.keymap.del("i", "@o")
-      vim.keymap.del("i", "@v")
-      vim.keymap.del("i", "@x")
+      vim.keymap.set("i", "->", "→", {nowait = true, buffer=true})
+      vim.keymap.set("i", "~>", "⇝", {nowait = true, buffer=true})
+      vim.keymap.set("i", "=>", "⇒", {nowait = true, buffer=true})
+      vim.keymap.set("i", "|>", "↦", {nowait = true, buffer=true})
+      vim.keymap.set("i", "@o", "☐", {nowait = true, buffer=true})
+      vim.keymap.set("i", "@v", "☑", {nowait = true, buffer=true})
+      vim.keymap.set("i", "@x", "☒", {nowait = true, buffer=true})
     end
   end
 })
